f16d241d6fa29dbbe11c52b526d6ad0752d0cce3
Richin Jain <richinjain@gmail.com>
2017-09-20 10:29:40 -0400

    Adding parameter support

--- downloadblob.sh
+++ downloadblob.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Downloading Azure CLI on the VSTS build agent machine
 apt-get update -y && apt-get install -y python libssl-dev libffi-dev python-dev build-essential
 curl -L https://azurecliprod.blob.core.windows.net/install.py -o install.py
 printf "/usr/azure-cli\n/usr/bin" | python install.py
 az
 
-export AZURE_STORAGE_ACCOUNT=devopsblogstorage
-export container_name=pretrainedmodel
-export AZURE_STORAGE_KEY=fzy27WL5j0c+N5/DepTkvfpUEJLz2c2S33q1SDuOh83cZKv3m7WMidKJD1ScKVFBWNZg4IKv1s7n7JCjmX4liQ==
+#Setting environment variables to access the blob container
+export AZURE_STORAGE_ACCOUNT=$1
+export AZURE_STORAGE_KEY=$2
+export container_name=$3
+export blob_name=$4
 
-az storage blob download --container-name $container_name --name ResNet_152.model --file downloadedmodelfile --output table
+# Downloading Blob
+az storage blob download --container-name $container_name --name $blob_name --file $blob_name --output table
 az storage blob list --container-name $container_name --output table
