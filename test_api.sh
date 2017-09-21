8bbbd78661c643adf505309aff882efaa1c73071
MSalvaris <msalvaris@users.noreply.github.com>
2017-09-21 10:25:53 +0100

    Trying to run docker from inside script

--- test_api.sh
+++ test_api.sh
@@ -2,6 +2,6 @@
 
 docker images
 docker ps -a
-docker run -p 88:88 acrforblog.azurecr.io/model-api:89
-
-#curl http://localhost:88/version
+docker run -d -p 88:88 acrforblog.azurecr.io/model-api:90
+sleep 20s
+curl http://0.0.0.0:88/version
