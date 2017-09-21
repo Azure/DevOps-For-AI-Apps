21f8320151dbf25abb8c94532f506135b932f281
Richin Jain <richinjain@gmail.com>
2017-09-21 00:32:53 -0400

    Running the docker image this time with right port

--- test_api.sh
+++ test_api.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run acrforblog.azurecr.io/model-api:latest
+docker run -d -p 8888:8888 acrforblog.azurecr.io/model-api:latest
 docker images
 docker ps -a
 curl http://localhost:8888/version
