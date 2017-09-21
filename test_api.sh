7bdec446e1c2e3f53f3be55bbd3125a994ee6aa6
Richin Jain <richinjain@gmail.com>
2017-09-21 00:28:00 -0400

    Changing the image version to latest

--- test_api.sh
+++ test_api.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run acrforblog.azurecr.io/model-api:$(Build.BuildId)
+docker run acrforblog.azurecr.io/model-api:latest
 docker images
 docker ps -a
 curl http://localhost:8888/version
