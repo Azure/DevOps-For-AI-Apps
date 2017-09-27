8c99a23b8c0f3c107b839d3c1a193828c7afb133
MSalvaris <msalvaris@users.noreply.github.com>
2017-09-27 12:11:50 +0100

    Updates api test

--- test_api.sh
+++ test_api.sh
@@ -6,19 +6,15 @@ echo "Starting Docker image " $IMAGE
 
 docker run -d -p 88:88 $IMAGE
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-
 sleep 10
 
 # Simple API test
 echo "Testing API"
 reply=$(curl http://0.0.0.0:88/version)
 if [[ $reply == 2.0rc1 ]]; then
-        echo -e "${GREEN}Successfully validated version API call ${NC}"
+        echo -e "Successfully validated version API call"
 else
-        echo "${RED}Basic API call failed ${NC}"
+        echo "Basic API call failed"
         exit 1
 fi
 
@@ -26,4 +22,5 @@ echo "Stopping container and removing"
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
+echo "Finished"
 exit 0
