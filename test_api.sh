fd175f8c82400ea092d6d3a9eb290a0328cb3598
MSalvaris <msalvaris@users.noreply.github.com>
2017-09-27 11:50:53 +0100

    Updates test api script: Adds some messages and color

--- test_api.sh
+++ test_api.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+# This script tests the docker image passed to it
 
-$IMAGE=$1
-
+IMAGE=$1
+echo "Starting Docker image " $IMAGE
 
 docker run -d -p 88:88 $IMAGE
-reply=$(curl http://0.0.0.0:88/version)# Simple API test
-if [ $reply == 2.0rc1 ]; then
-	echo "Successfully validated version API call"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+sleep 10
+
+# Simple API test
+echo "Testing API"
+reply=$(curl http://0.0.0.0:88/version)
+if [[ $reply == 2.0rc1 ]]; then
+        echo -e "${GREEN}Successfully validated version API call ${NC}"
 else
-	echo "Basic API call failed"
-	exit 1
+        echo "${RED}Basic API call failed ${NC}"
+        exit 1
 fi
+
+echo "Stopping container and removing"
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
