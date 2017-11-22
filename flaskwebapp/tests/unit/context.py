d25ec574444288fd5c3e5661a33ced2e49584d6b
MSalvaris <msalvaris@users.noreply.github.com>
2017-11-22 15:37:19 +0000

    Updates tests

--- flaskwebapp/tests/unit/context.py
+++ flaskwebapp/tests/unit/context.py
@@ -2,6 +2,5 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 
-print(sys.path)
 from flaskwebapp.driver import init, run
 from flaskwebapp import driver
